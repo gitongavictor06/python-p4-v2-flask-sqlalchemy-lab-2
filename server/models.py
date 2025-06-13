ef85fd796ed5531b7a1fc110184221f752289fa7
Victor <victor.gitonga1@student.moringaschool.com>
2025-06-13 12:34:38 +0300

    -changes

--- server/models.py
+++ server/models.py
@@ -1,4 +1,4 @@
-rom flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
